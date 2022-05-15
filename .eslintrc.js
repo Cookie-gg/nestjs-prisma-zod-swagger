4efd968b7eaab01e02a41eae4c6cc3eab3bd99df
Cookie_gg <cookie.gg.dev@gmail.com>
2022-05-15 22:36:19 +0900

    update .eslint.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -13,12 +13,11 @@ module.exports = {
     node: true,
     jest: true,
   },
-  ignorePatterns: ['.eslintrc.js'],
+  ignorePatterns: ['.eslintrc.js', '*.config.js'],
   rules: {
     '@typescript-eslint/interface-name-prefix': 'off',
     '@typescript-eslint/explicit-function-return-type': 'off',
     '@typescript-eslint/explicit-module-boundary-types': 'off',
-    '@typescript-eslint/no-explicit-any': 'off',
-    'prettier/prettier': ['error', { printWidth: 100 }],
+    'prettier/prettier': ['error', { printWidth: 120 }],
   },
 };
