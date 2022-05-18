7e7882455cb1e9c3c5ec78944d12ace0a4a9b34e
Cookie_gg <cookie.gg.dev@gmail.com>
2022-05-19 00:37:19 +0900

    lint

--- src/types/addons.d.ts
+++ src/types/addons.d.ts
@@ -8,11 +8,7 @@ type OptionalKeys<T> = {
   [K in keyof T]-?: {} extends { [P in K]: T[K] } ? K : never;
 }[keyof T];
 
-type IfEquals<X, Y, A = X, B = never> = (<T>() => T extends X ? 1 : 2) extends <T>() => T extends Y
-  ? 1
-  : 2
-  ? A
-  : B;
+type IfEquals<X, Y, A = X, B = never> = (<T>() => T extends X ? 1 : 2) extends <T>() => T extends Y ? 1 : 2 ? A : B;
 
 type WritableKeys<T> = {
   [P in keyof T]-?: IfEquals<{ [Q in P]: T[P] }, { -readonly [Q in P]: T[P] }, P>;
